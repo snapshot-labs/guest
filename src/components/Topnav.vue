866718474dc85a0b5e6b0d9a2883433e5f9c4136
bonustrack <fabien@bonustrack.co>
2022-03-20 23:14:46 +0700

    Fix border issue

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -3,7 +3,7 @@
     <div class="border-b ml-[32px] h-[64px]" />
     <router-link
       :to="{ name: 'home' }"
-      class="fixed top-0 left-0 w-[64px] h-[64px] block"
+      class="fixed top-0 left-0 w-[64px] h-[64px] block z-10"
     >
       <div class="ml-[20px] mt-3">
         <Icon name="emoji" :size="28" />
