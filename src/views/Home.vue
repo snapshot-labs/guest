0decd4c8e942f27eb2b693efb4983f086f17bf82
antiyro <antiyro@gmail.com>
2023-03-07 13:36:14 +0100

    Cleared stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,6 @@ const client = new GraphQLClient(endpoint);
     const accountTokens = ref([]);
 
     async function handleSearch() {
-      console.log('test')
       const query = `
         query GetTokens($accountAddress: String!) {
           accounttokens(where: { account: $accountAddress }) {
@@ -34,7 +33,7 @@ const client = new GraphQLClient(endpoint);
           }
         }
       `;
-      const variables = { accountAddress: accountAddress.value };
+      const variables = { accountAddress: accountAddress.value.slice(2) };
       const data = await client.request(query, variables);
       accountTokens.value = data.accounttokens;
 
@@ -47,7 +46,7 @@ const client = new GraphQLClient(endpoint);
   <div class="space-y-4">
     <div>
       <div class="align">
-        <h2 class="mb-2">Search for Starknet token holders</h2>
+        <h2>Search for Starknet token holders</h2>
       </div>
       <p class="mb-2 max-w-2xl">
         With Checkpoint it is possible to retrieve and index, quickly and easily, any data from Starknet, whether it is global or from a particular smart contract.<br><br> Witness the power of Checkpoint now by retrieving token holdings from any Starknet holder. (See how it works <a href="https://github.com/snapshot-labs/token-api-checkpoint" style="color: #374aff;">here</a>)
@@ -61,18 +60,20 @@ const client = new GraphQLClient(endpoint);
           />
           <UiButton type="submit" class="w-full">Search</UiButton>
         </form>
-      <ul class=" x-block">
-        <li class="token-item">
-          <span class="token-name">Name</span>
-          <span class="token-symbol">Symbol</span>
-          <span class="token-balance">Balance</span>
-        </li>
-        <li v-for="token in accountTokens" :key="token.id" class="token-item">
-          <span class="token-name">{{ token.token.name }}</span>
-          <span class="token-symbol">{{ token.token.symbol }}</span>
-          <span class="token-balance">{{ token.balance }}</span>
-        </li>
-      </ul>
+        <ul class="token-list x-block">
+          <li class="token-item token-header">
+            <span class="token-name">Name</span>
+            <span class="token-symbol">Symbol</span>
+            <span class="token-balance">Balance</span>
+            <span class="token-last">Last</span>
+          </li>
+          <li v-for="token in accountTokens" :key="token.id" class="token-item">
+            <span class="token-name">{{ token.token.name.length > 20 ? token.token.name.slice(0, 10) + '...' : token.token.name }}</span>
+            <span class="token-symbol">{{ token.token.symbol }}</span>
+            <span class="token-balance">{{ token.balance }}</span>
+            <a :href="`https://starkscan.co/tx/${token.tx}`"><span class="token-last">{{ `${token.tx.slice(0, 4)}...${token.tx.slice(-3)}` }}</span></a>
+          </li>
+       </ul>
   </div>
 </template>
 
@@ -81,30 +82,32 @@ const client = new GraphQLClient(endpoint);
     list-style: none;
     padding: 0;
     margin: 0;
-    width: 27%;
+    width: 100%;
   }
 
-  .token-item {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: #f7f7f7;
-    border-radius: 10px;
-    padding: 10px;
-    margin-bottom: 10px;
-  }
+.token-item {
+  display: grid;
+  grid-template-columns: 2fr 1fr 1fr 1fr;
+  grid-gap: 10px;
+  background-color: #f7f7f7;
+  border-radius: 10px;
+  padding: 10px;
+  margin-bottom: 10px;
+}
 
-  .token-name {
-    font-weight: bold;
-    margin-right: 10px;
-  }
+.token-item a{
+  color: #374aff;
+}
 
-  .token-balance {
-    font-size: 1.2rem;
-    font-weight: bold;
-  }
 
-  .token-symbol {
-    margin-right: 5px;
-  }
+
+.token-header {
+  font-weight: bold;
+}
+
+.token-balance {
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+
 </style>
